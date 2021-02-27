fc52a0538f35682b7db363c0237e083222e7146e
AndreiTrubinov <hiddengard@gmail.com>
2021-02-27 21:51:06 +0500

    ended project

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,30 @@
 $(function(){
 
+    $("a.click").click(function () {
+		elementClick = $(this).attr("href");
+		destination = $(elementClick).offset().top;
+		$("body,html").animate({scrollTop: destination }, 700);
+	});
 
+	$('.catalog-list').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		cssEase: 'linear',
+		appendArrows: $('.catalog-arrows'),
+		nextArrow: '<img src="images/arrow-right.svg" class="slick-next" alt="">',
+    	prevArrow: '<img src="images/arrow-left.svg" class="slick-prev" alt="">',
+	  });
+
+	  $('.comments-list').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		appendArrows: $('.comments-arrows'),
+		nextArrow: '<img src="images/arrow-right.svg" class="slick-comments-next" alt="">',
+    	prevArrow: '<img src="images/arrow-left.svg" class="slick-comments-prev" alt="">',
+	  });
+	  
 
 });
